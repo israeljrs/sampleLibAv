73539ade367f0e50906455124a93aabb0222f580
israeljrs <israeljrs@gmail.com>
2014-09-23 10:58:36 -0300

    Initial Commit

---
+++ sampleLibAV/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sampleLibAV
+//
+//  Created by Israel Junior on 23/09/14.
+//  Copyright (c) 2014 Israel Junior. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
