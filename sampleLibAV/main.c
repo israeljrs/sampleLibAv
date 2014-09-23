f0a3f9ae1778227a193dc8547486b6353c430534
israeljrs <israeljrs@gmail.com>
2014-09-23 11:16:06 -0300

    Initial ajustments to code.

--- sampleLibAV/main.c
+++ sampleLibAV/main.c
@@ -7,12 +7,33 @@
 //
 
 #include <stdio.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    char file[] = "/Users/israeljrs/Movies/Hillsong-At-the-Cross.mp4";
+    
+    AVFormatContext *pFormatCtx = NULL;
+    av_register_all();
+    
+    if (avformat_open_input(&pFormatCtx, file, NULL, NULL) != 0) {
+        printf("Formato de arquivo não suportado !!!\n");
+        return -1;
+    }
+    
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+        printf("Falha na pesquisa por streaming !!!\n\n");
+        return 1;
+    }
+    
+    av_dump_format(pFormatCtx, 0, file, 0);
+    
+    avformat_close_input(&pFormatCtx);
+    
+    printf("Sample the use of FFmpeg API !!!\n");
     return 0;
 }
 
